5cacd54610c259b337511449ec034eeaeb8c1be3
Gonzalo Murga C <gonzalomurga@hotmail.com>
2020-09-01 18:58:38 -0400

    Agregar componente resultado
    Agregar textview
    Correjir algunos errores en toast's

--- Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraApp/MainActivity.java
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraApp/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText n1Txt;
     private EditText n2Txt;
+    private TextView resultadoTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.n1Txt = findViewById(R.id.n1_txt);
         this.n2Txt = findViewById(R.id.n2_txt);
+        this.resultadoTxt = findViewById(R.id.res_txt);
     }
     private double calcular(int n1, int n2, String op)throws Exception{
         double resultado=0;
@@ -41,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         try {
             double suma = calcular(n1,n2, "suma");
             Toast.makeText(this,"La suma es: "+suma,Toast.LENGTH_LONG).show();
+            this.resultadoTxt.setText(""+suma);
+
 
         }catch (Exception e){
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
@@ -55,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         try {
         double resta = calcular(n1,n2, "resta");
         Toast.makeText(this,"La resta es: "+resta,Toast.LENGTH_LONG).show();
-
+        this.resultadoTxt.setText(""+resta);
         }catch (Exception e){
         Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         }
@@ -68,8 +73,8 @@ public class MainActivity extends AppCompatActivity {
         n2 = Integer.parseInt(this.n2Txt.getText().toString());
         try {
             double multi = calcular(n1,n2, "multiplicacion");
-            Toast.makeText(this,"La resta es: "+multi,Toast.LENGTH_LONG).show();
-
+            Toast.makeText(this,"La multiplicacion es: "+multi,Toast.LENGTH_LONG).show();
+            this.resultadoTxt.setText(""+multi);
         }catch (Exception e){
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         }
@@ -83,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             double div = calcular(n1,n2, "division");
             Toast.makeText(this,"La division es: "+div,Toast.LENGTH_LONG).show();
-
+            this.resultadoTxt.setText(""+div);
         }catch (Exception e){
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         }
